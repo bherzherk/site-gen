0b8e4707feea8ae14e4213d5e3fea4f3c5f630b4
bherzherk <bherzherk@gmail.com>
2025-11-01 21:19:03 -0400

    Adding split_delimiter for markdown in line

---
+++ src/split_delimiter.py
@@ -0,0 +1,26 @@
+from textnode import TextNode, TextType
+
+
+def split_nodes_delimiter(
+    old_nodes: list["TextNode"], delimiter: str, text_type: TextType
+):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+
+        split_nodes = []
+        parts = old_node.text.split(delimiter)
+        if len(parts) % 2 == 0:
+            raise ValueError("Invalid format, in markdown: section not closed")
+        for i in range(len(parts)):
+            if parts[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(parts[i], TextType.TEXT))
+            else:
+                split_nodes.append(TextNode(parts[i], text_type))
+
+        new_nodes.extend(split_nodes)
+    return new_nodes
