21d76d4c2ae6e736287714c471f44b828c7bb7f9
bherzherk <bherzherk@gmail.com>
2025-11-04 19:39:47 -0400

    Adding: test for split images and links

--- src/test_split_delimiter.py
+++ src/test_split_delimiter.py
@@ -1,5 +1,5 @@
 import unittest
-from split_delimiter import split_nodes_delimiter
+from split_delimiter import split_nodes_delimiter, split_nodes_image, split_nodes_link
 from textnode import TextNode, TextType
 
 
@@ -61,6 +61,78 @@ class TestSplitDelimiter(unittest.TestCase):
                 TextNode("This is the output: ", TextType.TEXT),
                 TextNode("hi", TextType.CODE),
             ],
+            new_nodes,
+        )
+
+    # Test Split image------------------------------------------------------------------------------------------
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
+    def test_split_img_error(self):
+        node = TextNode(
+            "This is an image: ![image](http://i.image.com to test raise error.",
+            TextType.TEXT,
+        )
+        with self.assertRaises(ValueError):
+            split_nodes_image([node])
+
+    def test_split_img_single(self):
+        node = TextNode("![image](http://img.com)", TextType.TEXT)
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("image", TextType.IMAGE, "http://img.com"),
+            ],
+            new_nodes,
+        )
+
+    # Test Split link------------------------------------------------------------------------------------------
+
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev) with text that follows",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("another link", TextType.LINK, "https://blog.boot.dev"),
+                TextNode(" with text that follows", TextType.TEXT),
+            ],
+            new_nodes,
+        )
+
+    def test_split_link_error(self):
+        node = TextNode(
+            "This is a text with link: [link](http://test.test this is a format error.",
+            TextType.TEXT,
+        )
+        with self.assertRaises(ValueError):
+            split_nodes_link([node])
+
+    def test_split_link_single(self):
+        node = TextNode("[link](http://link.com)", TextType.TEXT)
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [TextNode("link", TextType.LINK, "http://link.com")],
             new_nodes,
         )
 
