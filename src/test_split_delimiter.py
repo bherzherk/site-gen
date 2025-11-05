53dff89f1495f1333ab7e2aebd032b9ea255a787
bherzherk <bherzherk@gmail.com>
2025-11-04 21:36:17 -0400

    Adding: test for text to textnode func

--- src/test_split_delimiter.py
+++ src/test_split_delimiter.py
@@ -1,5 +1,10 @@
 import unittest
-from inline_mk import split_nodes_delimiter, split_nodes_image, split_nodes_link
+from inline_mk import (
+    split_nodes_delimiter,
+    split_nodes_image,
+    split_nodes_link,
+    text_to_textnodes,
+)
 from textnode import TextNode, TextType
 
 
@@ -136,8 +141,27 @@ class TestSplitDelimiter(unittest.TestCase):
             new_nodes,
         )
 
-
-# Test text to nodes----------------------------------------------------------------------------
+    # Test text to nodes----------------------------------------------------------------------------
+    def test_text_to_nodes(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode(
+                    "obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"
+                ),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            nodes,
+        )
 
 
 if __name__ == "__main__":
