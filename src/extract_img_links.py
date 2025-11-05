907c4c2437f602b37126e1089526e325d49138a0
bherzherk <bherzherk@gmail.com>
2025-11-04 21:23:38 -0400

    Removing: raise error, and conditional

--- src/extract_img_links.py
+++ src/extract_img_links.py
@@ -3,13 +3,9 @@ import re
 
 def extract_markdown_images(text: str):
     matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
-    if matches:
-        return matches
-    raise ValueError("Image in markdown not found!")
+    return matches
 
 
 def extract_markdown_links(text: str):
     matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
-    if matches:
-        return matches
-    raise ValueError("Link in markdown not found!")
+    return matches
