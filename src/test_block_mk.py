dd725990e78367f8c15836ba4b063881af2af2da
bherzherk <bherzherk@gmail.com>
2025-11-04 22:08:20 -0400

    Adding: tests for block_mk func

---
+++ src/test_block_mk.py
@@ -0,0 +1,52 @@
+import unittest
+from block_mk import markdown_to_blocks
+
+
+class TestBlockMk(unittest.TestCase):
+    def test_block_mk(self):
+        md = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+            blocks,
+        )
+
+    def test_markdown_to_blocks_newlines(self):
+        md = """
+This is **bolded** paragraph
+
+
+
+
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
