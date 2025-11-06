2967f764e773cd9bd3b3865e72a7a37baefc4104
bherzherk <bherzherk@gmail.com>
2025-11-05 22:10:05 -0400

    Fix: adding output type in text to textnodes func

--- src/inline_mk.py
+++ src/inline_mk.py
@@ -102,7 +102,7 @@ def split_nodes_link(old_nodes: list["TextNode"]):
 
 
 # function all in one:
-def text_to_textnodes(text):
+def text_to_textnodes(text) -> list["TextNode"]:
     nodes = [TextNode(text, TextType.TEXT)]
     nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
     nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
