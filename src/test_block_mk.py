8a3cf3b8176bb1003eab34176e66c6747f173e10
bherzherk <bherzherk@gmail.com>
2025-11-05 20:59:45 -0400

    Adding: tests for block to block type func

--- src/test_block_mk.py
+++ src/test_block_mk.py
@@ -1,5 +1,5 @@
 import unittest
-from block_mk import markdown_to_blocks
+from block_mk import markdown_to_blocks, block_to_block_type, BlockType
 
 
 class TestBlockMk(unittest.TestCase):
@@ -46,6 +46,74 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    # test block type----------------------------------------------------------------------------
+    def test_heading(self):
+        block = "##  this is header  "
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.HEADING)
+
+    def test_code(self):
+        block = """```python
+print('hello')
+```"""
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.CODE)
+
+    def test_quote(self):
+        block = "> this is a quote. "
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.QUOTE)
+
+    def test_multi_quotes(self):
+        block = """> this
+> is a
+> multiple quotes
+>example"""
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.QUOTE)
+
+    def test_unordered_list(self):
+        block = """- this
+- is a list"""
+
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.UNORDERED_LIST)
+
+    def test_multi_unroderded_list(self):
+        block = """- First item
+- Second item
+    - Nested item
+- Another item
+- Yet another"""
+
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.UNORDERED_LIST)
+
+    def test_ordered_list(self):
+        block = "1. this is\n2. an ordered\n3. list"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.ORDERED_LIST)
+
+    def test_nested_ordered_list(self):
+        block = "1. this is\n   1. nested list\n2. to test."
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.ORDERED_LIST)
+
+    def test_paragraph(self):
+        block = "this is a paragraph"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_paragraph_unmatch_case_1(self):
+        block = "1. This is\n2. a test\nto unmatch list"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_paragraph_unmatch_case_2(self):
+        block = "- this is\n- a test\n to unmatch list"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
 
 
 if __name__ == "__main__":
