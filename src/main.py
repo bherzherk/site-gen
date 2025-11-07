a61134a1e2bc531fa418f57fa561ac99bacd92ee
bherzherk <bherzherk@gmail.com>
2025-11-06 22:50:04 -0400

    Adding: copying files and folder funcs

--- src/main.py
+++ src/main.py
@@ -1,9 +1,39 @@
-from textnode import TextType, TextNode
+import os
+import shutil
+
+from textnode import TextNode, TextType
 
 
 def main():
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(node)
+    source = "./static"
+    destination = "./public"
+    copy_content_to_dir(source, destination)
+
+
+def copy_content_to_dir(source: str, destination: str):
+    delete_folder_content(destination)
+    copy_content(source, destination)
+
+
+def delete_folder_content(folder):
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+        else:
+            shutil.rmtree(file_path)
+
+
+def copy_content(source, destination):
+    items = os.listdir(source)
+    for item in items:
+        source_path = os.path.join(source, item)
+        if os.path.isfile(source_path):
+            shutil.copy(source_path, os.path.join(destination, item))
+        else:
+            destination_path = os.path.join(destination, item)
+            os.mkdir(destination_path)
+            copy_content(source_path, destination_path)
 
 
 if __name__ == "__main__":
